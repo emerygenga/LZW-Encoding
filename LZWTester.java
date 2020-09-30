6a8161d4957491d33ffb72cb61eba03fcf026e98
emerygenga <70218311+emerygenga@users.noreply.github.com>
2020-09-29 18:19:03 -0700

    Tester now prompts user to enter file to decode so multiple files can be tested without having to change the actual code. Also, a message is now printed when the encoding and decoding finish.

--- LZWTester.java
+++ LZWTester.java
@@ -3,12 +3,23 @@ import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.RandomAccessFile;
+import java.util.Scanner;
 
-public class LZWTester {
+public class LZWTester 
+{
 	public static void main (String [] args) throws IOException
 	{
 		Encoder en = new Encoder ();
-		en.encode("lzw-file1.txt");
+		Scanner scanner = new Scanner (System.in) ;
+		String fileName; 
+
+		System.out.println ("Enter the file you wish to decode: ");
+		fileName = scanner.nextLine (); 
+
+		en.encode(fileName);
+		System.out.println ("encoding finished"); 
+		
 		en.decode();
-		}
+		System.out.println ("decoding finished"); 
+	}
 }
